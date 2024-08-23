959a88686f966587d8cb0a9ad39c5359aee94bc6
Kalichuza <kalichuza@proton.me>
2024-08-23 14:19:09 -0400

    Update 64SERunner.psm1

--- 64SERunner.psm1
+++ 64SERunner.psm1
@@ -15,7 +15,20 @@ function Encode-ScriptToBase64 {
 
         # Use 64SE to encode the script to Base64
         Import-Module 64SE
-        Encode-FileToBase64 -FilePath $ScriptPath
+
+        # Read the script content
+        $scriptContent = Get-Content -Path $ScriptPath -Raw
+        
+        # Encode the script content
+        $base64String = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($scriptContent))
+        
+        # Generate the new file path with .64SE appended before the extension
+        $encodedFilePath = [System.IO.Path]::ChangeExtension($ScriptPath, ".64SE" + [System.IO.Path]::GetExtension($ScriptPath))
+        
+        # Write the Base64 string to the new file
+        [System.IO.File]::WriteAllText($encodedFilePath, $base64String)
+
+        Write-Output "Encoded file created at $encodedFilePath"
     }
 }
 
@@ -47,7 +60,10 @@ function EncodeAndRun-Script {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
-        [string]$ScriptPath
+        [string]$ScriptPath,
+
+        [Parameter(Position = 1)]
+        [hashtable]$ScriptArgs
     )
 
     process {
@@ -59,13 +75,33 @@ function EncodeAndRun-Script {
         # Encode the script
         Encode-ScriptToBase64 -ScriptPath $ScriptPath
 
-        # Generate the path for the encoded script
-        $encodedScriptPath = "$ScriptPath.64SE"
+        # Generate the path for the encoded script with .64SE before the extension
+        $encodedScriptPath = [System.IO.Path]::ChangeExtension($ScriptPath, ".64SE" + [System.IO.Path]::GetExtension($ScriptPath))
 
-        # Run the encoded script
-        Run-Base64EncodedScript -EncodedScriptPath $encodedScriptPath
+        # Read the Base64 encoded content
+        $base64EncodedScript = Get-Content -Path $encodedScriptPath -Raw
+        
+        # Decode the Base64 string
+        $decodedScript = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($base64EncodedScript))
+
+        # Prepare the arguments string
+        $argsString = ""
+        if ($ScriptArgs) {
+            foreach ($key in $ScriptArgs.Keys) {
+                $argsString += " -$key '$($ScriptArgs[$key])'"
+            }
+        }
+
+        # Use Invoke-Command to run the decoded script with arguments
+        Invoke-Command -ScriptBlock ([ScriptBlock]::Create($decodedScript)) -ArgumentList ($argsString.Split())
     }
 }
+
+
+
+
+
+
 
 # Exporting functions
 Export-ModuleMember -Function Encode-ScriptToBase64, Run-Base64EncodedScript, EncodeAndRun-Script
